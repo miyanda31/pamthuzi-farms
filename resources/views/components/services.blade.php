4038b24228ea6bf7f73b680cce5a23f5b2738a98
Augustine Mlowoka <chirwasekelerani@gmail.com>
2024-05-16 12:23:21 +0200

    Minor changes 2

--- resources/views/components/services.blade.php
+++ resources/views/components/services.blade.php
@@ -6,38 +6,90 @@
                     <div class="row">
                         <div class="col-lg-4">
                             <div class="services__details__product__item">
-                                <h4><span>01.</span> Enterprise startup and growth</h4>
-                                <p>Business plan development and general advice</p>
+                                <h4><span>01.</span> Enterprise Startup and Expansion</h4>
+                                <ul style="color: #5c5c5c" class="list-unstyled text-justify">
+                                    <li class="mb-2 mt-2"><strong>Strategic Business Planning:</strong> Crafting tailored business plans to drive startup success and foster growth.</li>
+                                    <li><strong>Expert Consultation:</strong> Offering seasoned advice to navigate the complexities of livestock business development.</li>
+                                </ul>
                             </div>
                         </div>
                         <div class="col-lg-4">
                             <div class="services__details__product__item">
-                                <h4><span>02.</span> Feed formulation</h4>
-                                <p>For farm owners, staff and individuals</p>
+                                <h4><span>02.</span> Advanced Feed formulation</h4>
+                                <ul style="color: #5c5c5c" class="list-unstyled text-justify">
+                                    <li class="mb-2 mt-2">
+                                        <strong>Nutritional Optimization:
+                                        </strong> Custom feed solutions for enhanced farm productivity.
+                                    </li>
+                                    <li>
+                                        <strong>Training Workshops:
+                                        </strong> Empowering farm owners, employees, and other stakeholders with the knowledge to innovate in feed composition.
+                                    </li>
+                                </ul>
                             </div>
                         </div>
                         <div class="col-lg-4">
                             <div class="services__details__product__item">
-                                <h4><span>03.</span> Animal Health</h4>
-                                <p>Prevention, screening and diagnosis</p>
+                                <h4><span>03.</span> Comprehensive Animal Health Services
+                                </h4>
+                                <ul style="color: #5c5c5c" class="list-unstyled text-justify">
+                                    <li class="mb-2 mt-2">
+                                        <strong>Proactive Wellness Programs:
+                                        </strong> Focusing on prevention to maintain herd health.
+                                    </li>
+                                    <li>
+                                        <strong> Diagnostic Excellence: </strong>Utilizing cutting-edge screening methods for accurate diagnosis.
+                                    </li>
+                                </ul>
                             </div>
                         </div>
                         <div class="col-lg-4">
                             <div class="services__details__product__item">
-                                <h4><span>04.</span> Proposal Development</h4>
-                                <p>We do it for you or guide you through it</p>
+                                <h4><span>04.</span> Expert Proposal Development
+                                </h4>
+                                <ul style="color: #5c5c5c" class="list-unstyled text-justify">
+                                    <li class="mb-2 mt-2">
+                                        <strong>Full-Service Proposal Creation:</strong>Let us handle the intricacies of proposal writing for you.
+
+                                    </li>
+                                    <li>
+                                        <strong>Guided Proposal Crafting:</strong>Providing step-by-step assistance to perfect your submissions.
+                                    </li>
+                                </ul>
                             </div>
                         </div>
                         <div class="col-lg-4">
                             <div class="services__details__product__item">
-                                <h4><span>05.</span> Livestock Program / Project Management</h4>
-                                <p>Implementation and evaluation</p>
+                                <h4><span>05.</span>  Livestock Program and Project Oversight
+                                </h4>
+                                <ul style="color: #5c5c5c" class="list-unstyled text-justify">
+                                    <li class="mb-2 mt-2">
+                                        <strong>Program Implementation: </strong>Executing livestock projects with precision and care.
+                                    </li>
+                                    <li><strong>Performance Evaluation: </strong>Measuring outcomes to ensure the highest standards of program success.
+                                    </li>
+                                </ul>
                             </div>
                         </div>
                         <div class="col-lg-4">
                             <div class="services__details__product__item">
-                                <h4><span>06.</span> Customized Services</h4>
-                                <p>Fire away</p>
+                                <h4><span>06.</span> Capacity Building</h4>
+                                <ul style="color: #5c5c5c" class="list-unstyled text-justify">
+                                    <li class="mb-2 mt-2">
+                                        <strong> For Farm Owners, Staff, and NGOs: </strong>This package is tailor-made to match your specific situation and requirements.
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="services__details__product__item">
+                                <h4><span>07.</span> Tailored Consulting Solutions</h4>
+                                <ul style="color: #5c5c5c" class="list-unstyled text-justify">
+                                    <li class="mb-2 mt-2">
+                                        <strong>Ask and You Shall be Assisted: </strong>Whatever your need, our bespoke services are designed to meet it.
+                                    </li>
+                                    <li></li>
+                                </ul>
                             </div>
                         </div>
                     </div>
