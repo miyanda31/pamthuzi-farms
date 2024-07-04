322b222feab86a44bf619951bdf316d74cbbfa01
Augustine Mlowoka <chirwasekelerani@gmail.com>
2024-07-04 17:37:20 +0900

    Updated team 2

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,8 +7,11 @@
     <meta content="Research, Consultancy, Farming, Implementation, Evaluation" name="keywords">
     <meta content="Farming with foresight, leading with research" name="description">
 
-    <!-- Favicon -->
-    <link href="img/favicon.ico" rel="icon">
+    <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png">
+    <link rel="manifest" href="/img/site.webmanifest">
+
 
     <!-- Google Font -->
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
